601eb01dab8e935e7a3f7c2b03cc7a30b7b8d8b3
maziarja <maziar.ja@gmail.com>
2026-06-20 12:38:45 -0700

    connect the frontend and backend

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>hello word</div>;
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/hello", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+  console.log(message);
+  return <div>from server {message}</div>;
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello world!");
+app.use(cors());
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "hello world" });
 });
 
 const port = process.env.PORT;
