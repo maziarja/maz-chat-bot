932fa9c587d8b7f958bb8621b4dfcfef3e9ddbc5
maziarja <maziar.ja@gmail.com>
2026-06-20 12:57:04 -0700

    Run both apps together

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+  {
+    name: "server",
+    command: "bun run dev",
+    prefixColor: "bgGreen",
+    cwd: "packages/server",
+  },
+  {
+    name: "client",
+    command: "bun run dev",
+    prefixColor: "bgMagenta",
+    cwd: "packages/client",
+  },
+]);
